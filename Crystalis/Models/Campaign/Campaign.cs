947b78a94159c469e8e60231f4778b99c3302be7
haqz <dev@haqz.org>
2025-04-09 20:36:02 +0200

    Added forgotten file in last commit

--- Crystalis/Models/Campaign/Campaign.cs
+++ Crystalis/Models/Campaign/Campaign.cs
@@ -12,7 +12,9 @@ public class Campaign
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
     public string AuthorId { get; set; }
+    public int WorldId { get; set; }
     
+    public World.World World { get; set; }
     public ICollection<Location> Locations { get; set; } = new List<Location>();
     public ICollection<CampaignUser> CampaignUsers { get; set; } = new List<CampaignUser>();
     public ICollection<CampaignQueue> CampaignQueues { get; set; } = new List<CampaignQueue>();
